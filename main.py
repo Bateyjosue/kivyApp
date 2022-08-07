bfbfc5f7cc61e1be1b41cf498b1e75b85fdb923b
Bateyjosue <josuebatey19@gmail.com>
2022-08-07 11:27:54 +0200

    Initial files

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+class BaseApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    BaseApp().run()
