1d33cfb410ea23e77a6ded5bec65c9bb78388acd
Bateyjosue <josuebatey19@gmail.com>
2022-08-07 14:12:46 +0200

    add Button and Label to the widget

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+"""Class representing Our Main Class"""
 
 from kivy.app import App
 from kivy.uix.widget import Widget
 
 
 class MainWidget(Widget):
-    pass
+    """Class representing Our main Widget"""
+
 
 class BaseApp(App):
-    pass
+    """Class representing Our Base Widget of the Apps"""
+
+    def build(self):
+        """Build Method for building the Base App Layout"""
+        return MainWidget()
 
 
 if __name__ == '__main__':
